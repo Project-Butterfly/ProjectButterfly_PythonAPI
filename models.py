093dc18f570bfb258887a01b9dcc4c0cce7ceab7
jaqxues <32979131+jaqxues@users.noreply.github.com>
2019-10-29 13:39:54 +0100

    Decoupling ModelClasses

---
+++ models.py
@@ -0,0 +1,18 @@
+class Message:
+    def __init__(self, **kwargs):
+        self.id = kwargs['id']
+        self.author = kwargs['author']['id']
+
+
+class Post:
+    def __init__(self, **kwargs):
+        self.id = kwargs["id"]
+        self.completed = kwargs["completed"]
+        self.reserved = kwargs["reserved"]
+
+
+class User:
+    def __init__(self, user_id, name, phone):
+        self.user_id = user_id
+        self.name = name
+        self.phone = phone
